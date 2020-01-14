685787c9354c8c2169df6d8708e2b5a140d248b0
Yeison Montoya <yeison.montoya@ingeneo.com.co>
2020-01-14 11:19:22 -0500

    ng for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Tproduct } from './product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,7 +16,52 @@ export class AppComponent {
     'Nicolas', 'Jualian', 'Perez'
   ];
 
-  products = [];
+  objeto = {};
+
+  products: Tproduct[] = [
+    {
+      id: '1',
+      image: 'assets/img/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/img/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/img/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/img/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/img/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/img/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+  ];
 
   addItem(nameUser) {
     this.items.push(nameUser);
---
+++ src/app/product.model.ts
@@ -0,0 +1,7 @@
+export interface Tproduct{
+    id: string;
+    image: string;
+    title: string;
+    price: number;
+    description: string;
+}
