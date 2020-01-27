136ae5a573a6a0562e6b9c4c26932028bd4c8bff
Yeison Montoya <yeison.montoya@ingeneo.com.co>
2020-01-27 13:26:52 -0500

    añadir componentes para la home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { DemoComponent } from './components/demo/demo.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +27,9 @@ import { DemoComponent } from './components/demo/demo.component';
     ProductsComponent,
     ContactComponent,
     DemoComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule,
