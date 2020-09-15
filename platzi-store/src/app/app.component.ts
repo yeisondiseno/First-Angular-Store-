87e8c8949f3f373cea3875b78bb82aea033987b0
Yeison Montoya <yeisondiseno@gmail.com>
2020-09-15 09:59:10 -0500

    Y.Montoya: first concepts

--- platzi-store/src/app/app.component.ts
+++ platzi-store/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'platzi-store';
+  title = 'Platzi Store';
+
+  items = ['Nicolas', 'Yeison', 'Julian'];
 }
