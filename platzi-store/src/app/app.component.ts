52a3423d36ac352dcbe8536c6b5d4c752cba9e6c
Yeison Montoya <yeisondiseno@gmail.com>
2020-09-15 10:17:05 -0500

    Y.Montoya: primeros conceptos

--- platzi-store/src/app/app.component.ts
+++ platzi-store/src/app/app.component.ts
@@ -9,4 +9,56 @@ export class AppComponent {
   title = 'Platzi Store';
 
   items = ['Nicolas', 'Yeison', 'Julian'];
+  products = [
+    {
+      id: '1',
+      image: 'assets/img/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/img/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/img/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/img/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/img/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/img/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    }
+  ];
+
+  addItem() {
+    this.items.push('nuevo Item');
+  }
+
+  deleteItem( index: number ) {
+    this.items.splice( index, 1 );
+  }
 }
