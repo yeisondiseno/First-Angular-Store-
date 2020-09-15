9e468745fee49bc50d1f9fe061294e16681f9bec
Yeison Montoya <yeisondiseno@gmail.com>
2020-09-15 15:31:52 -0500

    Y.Montoya: Output e Input, comunicación entre componentes

--- platzi-store/src/app/app.component.ts
+++ platzi-store/src/app/app.component.ts
@@ -63,4 +63,10 @@ export class AppComponent {
   deleteItem( index: number ) {
     this.items.splice( index, 1 );
   }
+
+
+  clickProduct( id: number ) {
+    console.log('product', id);
+  }
+
 }
--- platzi-store/src/app/components/product.component.ts
+++ platzi-store/src/app/components/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Product } from '../product.model';
 
@@ -8,13 +8,11 @@ import { Product } from '../product.model';
 })
 
 export class ProductComponent {
-    products: Product[] = [
-        {
-            id: '1',
-            image: '../assets/img/camiseta.png',
-            title: 'Camiseta',
-            price: 80000,
-            description: 'bla bla bla bla bla'
-        }
-    ];
-}
+    @Input() product: Product;
+    @Output() productClicked = new EventEmitter<any>();
+
+    addCart() {
+        console.log('Agregado al carro');
+        this.productClicked.emit(this.product.id);
+    }
+};
