0ac19260e32eb4b67995e7afbf9801215b5cf16a
Yeison Montoya <yeisondiseno@gmail.com>
2020-09-15 15:49:19 -0500

    Y.Montoya: siclos de vida

--- platzi-store/src/app/components/product.component.ts
+++ platzi-store/src/app/components/product.component.ts
@@ -1,4 +1,14 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { 
+    Component, 
+    Input, 
+    Output, 
+    EventEmitter, 
+    OnChanges, 
+    SimpleChanges, 
+    OnInit, 
+    DoCheck, 
+    OnDestroy 
+} from '@angular/core';
 
 import { Product } from '../product.model';
 
@@ -7,9 +17,30 @@ import { Product } from '../product.model';
     templateUrl: './product.component.html'
 })
 
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
     @Input() product: Product;
     @Output() productClicked = new EventEmitter<any>();
+
+    constructor() {
+        console.log('constructor');
+    }
+
+    // ngOnChanges(changes: SimpleChanges): void {
+    //     console.log('ngOnChanges');
+    //     console.log(changes);
+    // }
+
+    ngOnInit() {
+        console.log('ngOnInit');
+    }
+
+    ngDoCheck() {
+        console.log('ngDoCheck');
+    }
+
+    ngOnDestroy() {
+        console.log('ngOnDestroy')
+    }
 
     addCart() {
         console.log('Agregado al carro');
