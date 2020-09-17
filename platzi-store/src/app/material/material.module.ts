dbaa13aefc6dec0179fe15990bc19af8f1ccc5f6
Yeison Montoya <yeisondiseno@gmail.com>
2020-09-17 12:37:07 -0500

    Y.Montoya: Cards

--- platzi-store/src/app/material/material.module.ts
+++ platzi-store/src/app/material/material.module.ts
@@ -2,15 +2,34 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatBadgeModule,
+    MatIconModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatCardModule
   ],
   imports: [
     CommonModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatBadgeModule,
+    MatIconModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
