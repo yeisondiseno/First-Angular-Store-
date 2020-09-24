f680fd26b2aba2615ac7321c1adb213dd0827b8b
Yeison Montoya <yeisondiseno@gmail.com>
2020-09-23 21:57:44 -0500

    Y.Montoya: Finalización del curso

--- platzi-store/src/app/components/admin/components/product-form/product-form.component.ts
+++ platzi-store/src/app/components/admin/components/product-form/product-form.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { MyValidators } from '../../../../utils/validators';
+
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs/internal/Observable';
 
 import { ProductsService } from '../../../../core/services/products.service';
 
@@ -13,11 +17,13 @@ import { ProductsService } from '../../../../core/services/products.service';
 export class ProductFormComponent {
 
   form: FormGroup;
+  image$: Observable<any>;
 
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage
     ) {
       this.buildForm();
   }
@@ -33,6 +39,28 @@ export class ProductFormComponent {
         this.router.navigate(['admin/listproducts']);
       });
     }
+  }
+
+  uploadFile( event ) {
+    const file = event.target.files[0];
+    const name = file.name;
+    // const dir = 'images';
+    const fileRef = this.storage.ref( name );
+    const task = this.storage.upload( name, file );
+    // console.log( name );
+    // console.log( file );
+
+    task.snapshotChanges()
+    .pipe(
+      finalize( () => {
+        this.image$ = fileRef.getDownloadURL();
+        this.image$.subscribe( url => {
+          console.log(url);
+          this.form.get('image').setValue(url);
+        } );
+      } )
+    )
+    .subscribe();
   }
 
   private buildForm() {
